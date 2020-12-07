78e306cef0222807d6ef7997784090d19a99bc0a
SayakaNakajima <andsayaka@gmail.com>
2020-12-07 22:54:22 +0900

    migration and seeding

---
+++ data/import.js
@@ -0,0 +1,113 @@
+const fs = require("fs");
+const db = require("../server/knex.js");
+
+(async () => {
+  try {
+    const recipes = JSON.parse(fs.readFileSync("./data/recipes.json"));
+
+    let [recipeCount] = await db("recipes").count("*");
+    let [ingridientCount] = await db("ingridients").count("*");
+    let [procedureCount] = await db("procedures").count("*");
+    let [recipe_ingridientCount] = await db("recipe_ingridient").count("*");
+
+    await db("recipes").del;
+    for (const recipe of recipes) {
+      const r_id = recipe.Id;
+      const r_title = recipe.Title;
+      const r_img = recipe.Img;
+      const r_time = recipe.Time;
+
+      const result = await db("recipes").insert({
+        r_id,
+        r_title,
+        r_img,
+        r_time,
+      });
+    }
+    
+    //// Insert Procedures Data
+    await db("procedures").del();
+
+    //// Make Procedures Data
+    const procedures = [];
+    for (const recipe of recipes) {
+      const p_recipe_id = recipe.Id;
+      recipe.Procedures.forEach((item) => {
+        let procedure ={
+          p_recipe_id,
+          p_order_num : item.OrderNum,
+          p_text : item.Text,
+        };
+        procedures.push(procedure);
+      });
+    }
+
+    //// Create Data in Database 
+    procedures.forEach(async (item) => {
+      await db("procedures").insert({
+        p_recipe_id: item.p_recipe_id,
+        p_order_num: item.p_order_num,
+        p_text: item.p_text,
+      });
+    });
+
+    
+    //// Insert Ingridients Data
+    //// Delete Datas
+    await db("ingridients").del();
+
+    //// Make Ingridients Data
+    const ingridients = [];
+    for (const recipe of recipes) {
+      recipe.Ingridients.forEach((item) => {
+        let ingridient = {
+          i_id: item.Id,
+          i_name: item.Name,
+          i_category: item.Category,
+        };
+        const found = ingridients.some((obj) => obj.i_id === ingridient.i_id);
+        if (!found) {
+          ingridients.push(ingridient);
+        }
+      });
+    }
+
+    //// Create Data in Database 
+    ingridients.forEach(async (item) => {
+      await db("ingridients").insert({
+        i_id: item.i_id,
+        i_name: item.i_name,
+        i_category: item.i_category,
+      });
+    });
+
+    ////Insert recipe_ingridient join table info
+    await db("recipe_ingridient").del();      
+
+    const ingridientInfos = [];
+    for (const recipe of recipes) {
+      const recipe_id = recipe.Id;
+      recipe.Ingridients.forEach((ingridient) => {
+        let ingridientInfo = {
+          recipe_id,
+          ingridient_id : ingridient.Id,
+          amount : ingridient.Amount,
+        }
+        ingridientInfos.push(ingridientInfo);
+      });
+    }
+
+    ingridientInfos.forEach(async (item) => {
+      await db("recipe_ingridient").insert({
+        recipe_id: item.recipe_id,
+        ingridient_id: item.ingridient_id,
+        amount: item.amount,
+      });
+    });
+
+    return console.log("Finished!");
+
+  } catch (err) {
+    console.error("Error inserting records", err);
+  }
+})();
---
+++ migrations/20201207212221_migrations03.js
@@ -0,0 +1,50 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable("recipes",  (t) => {
+            t.increments("r_id").index();
+            t.string("r_title").notNullable();
+            t.string("r_img");
+            t.string("r_time");
+        })
+        .createTable("ingridients", (t) => {
+            t.increments("i_id").index();
+            t.string("i_name").notNullable();
+            t.string("i_category");
+        })
+        .createTable("recipe_ingridient", (t) =>{
+            t
+                .integer("recipe_id")
+                .references("r_id")
+                .inTable("recipes")
+                .notNull()
+                .onDelete("cascade");
+            t
+                .integer("ingridient_id")
+                .references("i_id")
+                .inTable("ingridients")
+                .notNull()
+                .onDelete("cascade");
+            t.string("amount").notNullable();
+        })
+        .createTable("procedures", (t) => {
+            t.increments("p_id").index();
+            t
+                .integer("p_recipe_id")
+                .references("r_id")
+                .inTable("recipes")
+                .notNull()
+                .onDelete("cascade");
+            t.string("p_order_num");
+            t.text("p_text");
+        })
+        
+};
+
+exports.down = function(knex) {
+    knex.schema
+        .dropTable("procedures")
+        .dropTable("recipe_ingridient")
+        .dropTable("ingridients")
+        .dropTable("recipes");
+};
